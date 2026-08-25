c4e9423d11278f520da0a0fb0816b38a9f41ba55
altonFreitas <8220760af@gmail.com>
2026-08-25 19:26:55 +0100

    remove stale apply-script backup snapshots

--- .v3-backup/src/app/checkout/page.tsx
+++
@@ -1,8 +0,0 @@
-import CheckoutForm from "@/components/CheckoutForm";
-import { getSettings } from "@/lib/data/public";
-import { getLang } from "@/lib/lang";
-
-export default async function CheckoutPage() {
-  const [lang, settings] = await Promise.all([getLang(), getSettings()]);
-  return <CheckoutForm lang={lang} settings={settings} />;
-}
